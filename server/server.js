39fb434ed89db5df0afc3e82521d60a5fb853eb7
Success <Success>
2019-11-07 18:43:17 +0100

    init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3030;
+
+let app = express();
+let server = http.createServer(app);
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, err =>
+  err ? console.log(err) : console.log(`Server started @port: ${port}`)
+);
