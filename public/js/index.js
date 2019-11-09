36dc85c88559d9e984ec090dbb3c8f372c448f1d
Success <Success>
2019-11-09 13:54:38 +0100

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,45 @@
 var socket = io();
+
 socket.on("connect", () => {
   console.log("Connected to server");
-
-  socket.on("newMessage", message => {
-    console.log("newMessage", message);
-    let li = $("<li></li>");
-    li.text(`${message.from}: ${message.text}`);
-    $("#messages").append(li);
-  });
 });
 
 socket.on("disconnect", () => {
   console.log("Disconnected from server");
 });
 
+// New message
+socket.on("newMessage", message => {
+  let formattedTime = moment(message.createdAt).format("h:mm a");
+  let li = $("<li></li>");
+  li.text(`${message.from}: ${formattedTime} ${message.text}`);
+  $("#messages").append(li);
+});
+
+let messageTextBox = $("[name=message]");
+
+// Message form
 $("#message-form").on("submit", e => {
   e.preventDefault();
 
   socket.emit(
     "createMessage",
-    { from: "User", text: $("[name=message]").val() },
+    { from: "User", text: messageTextBox.val() },
     data => {
       // console.log("Seen it, ", data);
+      messageTextBox.val("");
     }
   );
 });
 
+// New location message
 socket.on("newLocationMessage", (locationMessage, callback) => {
+  let formattedTime = moment(message.createdAt).format("h:mm a");
   let li = $("<li></li>");
   let link = $(`<a target="_blank">My current location</a>`);
 
   link.attr("href", locationMessage.url);
-  li.text(`${locationMessage.from}: `);
+  li.text(`${locationMessage.from}: ${formattedTime} `);
   li.append(link);
   $("#messages").append(li);
 });
@@ -43,16 +51,15 @@ locationButton.on("click", e => {
     return alert("Geolocation not supported by your browser!");
   }
 
+  locationButton.attr("disabled", "disabled").text("Sending location...");
   navigator.geolocation.getCurrentPosition(
     position => {
       const { latitude, longitude } = position.coords;
       socket.emit("createLocationMessage", { latitude, longitude });
+      locationButton.removeAttr("disabled").text("Send location");
     },
     () => {
-      // socket.emit("createLocationMessage", {
-      //   latitude: "1212",
-      //   longitude: "-234545"
-      // });
+      locationButton.removeAttr("disabled").text("Send location");
       alert("Unable to fetch location!.");
     }
   );
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,14 @@
+const moment = require("moment");
+
 let generateMessage = (from, text) => {
-  return { from, text, createdAt: new Date().getTime() };
+  return { from, text, createdAt: moment().valueOf() };
 };
 
 let generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
     url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   };
 };
 
